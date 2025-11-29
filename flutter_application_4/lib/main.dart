acddac128e43a85608bf18dbfdd794c37d00a532
Piero Poblete <ppobletea@ulasalle.edu.pe>
2025-11-28 21:05:33 -0500

    Subiendo código principal.

---
+++ flutter_application_4/lib/main.dart
@@ -0,0 +1,32 @@
+// Descripción: Main
+// Autor: Piero Poblete
+// Fecha creación: 19/11/2025
+// Última modificación: 19/11/2025
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'viewmodels/user_view_model.dart';
+import 'views/login_screen.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => UserViewModel(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Gestión de Usuarios',
+      theme: ThemeData(primarySwatch: Colors.indigo),
+      
+      home: const LoginScreen(),
+    );
+  }
+}
---
+++ flutter_application_4/lib/views/login_screen.dart
@@ -0,0 +1,121 @@
+// Descripción: Muestra el login
+// Autor: Piero Poblete
+// Fecha creación: 19/11/2025
+// Última modificación: 19/11/2025
+
+import 'package:flutter/material.dart';
+import 'user_list_screen.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  String _email = '';
+  String _password = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Login')),
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+
+              // EMAIL
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Correo',
+                  prefixIcon: Icon(Icons.email),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'El correo es obligatorio';
+                  }
+                  if (!value.contains('@')) {
+                    return 'Debe ser un correo válido';
+                  }
+                  return null;
+                },
+                onSaved: (value) => _email = value!,
+              ),
+
+              const SizedBox(height: 16),
+
+              // PASSWORD
+              TextFormField(
+                obscureText: true,
+                decoration: const InputDecoration(
+                  labelText: 'Contraseña',
+                  prefixIcon: Icon(Icons.lock),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'La contraseña es obligatoria';
+                  }
+                  if (value.length < 6) {
+                    return 'Debe tener al menos 6 caracteres';
+                  }
+                  return null;
+                },
+                onSaved: (value) => _password = value!,
+              ),
+
+              const SizedBox(height: 12),
+
+              // TEXTO "¿Olvidaste tu contraseña?"
+              Align(
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                  onPressed: () {
+                  },
+                  child: const Text(
+                    "¿Olvidaste tu contraseña?",
+                    style: TextStyle(fontSize: 14),
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 24),
+
+              // BOTÓN INGRESAR
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _formKey.currentState!.save();
+
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => UserListScreen(email: _email),
+                      ),
+                    );
+                  }
+                },
+                child: const Text('Ingresar'),
+              ),
+
+              const SizedBox(height: 12),
+
+              // BOTÓN "CREAR CUENTA"
+              OutlinedButton(
+                onPressed: () {
+                },
+                child: const Text("Crear cuenta"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
